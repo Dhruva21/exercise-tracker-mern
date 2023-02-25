72299440d2e0baa730b025e2a0a4b6bdc986f5e3
dhruvaindavarapu <idhruva@horizon.csueastbay.edu>
2023-02-24 19:12:48 -0800

    finished code

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='container'>
+        <Navbar />
+          <br />
+        <Routes>
+          
+          <Route path="/"  element={<ExercisesList/ >}></Route>
+          <Route path="/edit/:id" element={<EditExercise />} ></Route>
+          <Route path="/create" element={<CreateExercise />} ></Route>
+          <Route path="/user" element={<CreateUser />} ></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
